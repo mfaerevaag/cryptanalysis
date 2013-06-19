8c4f845a4aeea11365b4b9d8d6493a67cae0271e
Markus Færevaag <mafaer@gmail.com>
2013-06-19 13:10:59 +0200

    Commented code in FreqAnalysis

--- project1/FreqAnalysis.py
+++ project1/FreqAnalysis.py
@@ -4,55 +4,63 @@ import os
 if __name__ == '__main__':
     os.system('clear')
 
+    # Check if enough argument were given
     argcount = 0
     for arg in argv:
-        argcount += 1
-        
+        argcount += 1    
     if argcount < 2:
         print "Error: No input file spesified"
         exit()
 
     script, inputfile = argv
 
-    input = open(inputfile, "r")
-    output1 = open("output1.txt", "w")
-    output2 = open("output2.txt", "w")
+    # Open files for reading and writing
+    input = open(inputfile, 'r')
+    output1 = open("output1.txt", 'w')
+    output2 = open("output2.txt", 'w')
 
+    # Declare variables to scope
     dict = {}
-    total = [0, 0]
+    count = [0, 0]
 
+    # Itereate lines in input file
     for line in input:
-        line = line.upper()
-        
-        for i in range(0, len(line)):
-            for j in range(1, 4):
-                ch = line[i:i+j]
-                
-                if not ch.isalpha():
-                    continue
-                
+        line = line.upper() # Clean line
+
+        # For every leagal permutation
+        for i in xrange(0, len(line)):
+            # Mono-, dia- and trigrams
+            for j in xrange(1, 4):
+                ch = line[i:i+j] # Substring w/ *gram
+
+                # Continue if not all letters
+                if not ch.isalpha(): continue 
+
+                # Lazy create and inc counter
                 if ch in dict:
                     dict[ch] += 1
                 else:
                     dict[ch] = 1
-                    
-                total[0 if len(ch) < 2 else 1] += 1
 
-    # Monograms
+                # Increment total number of *grams
+                count[0 if len(ch) < 2 else 1] += 1
+
+    # Print all monograms to file
     for key, value in sorted(dict.iteritems()):
-        if len(key) > 1:
-            continue
+        if len(key) > 1: continue
         output1.write("{0}: {2:.3%}\n".format(
-                key, value, float(value)/float(total[0])))
+                key, value, float(value)/float(count[0])))
 
-    # Di- and trigrams
-        for key, value in sorted(dict.iteritems(), 
-                                 key=lambda (k, v): (v, k), reverse=True):
-            if len(key) <= 1:
-                continue
-            output2.write("{0}: {2:.3%}\n".format(
-                    key, value, float(value)/float(total[0])))
-  
+    # Print all di- and trigrams to file
+    for key, value in sorted(dict.iteritems(), 
+                             key=lambda (k, v): (v, k), reverse=True):
+        if len(key) <= 1: continue
+        output2.write("{0}: {2:.3%}\n".format(
+                key, value, float(value)/float(count[0])))
+
+    print "Successfully analyzed %s" % inputfile
+    
+    # Close files before finish
     input.close()
     output1.close()
     output2.close()
