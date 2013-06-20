c98fd3263e0f137e504805800b1e4b7b00edb09e
Markus Færevaag <mafaer@gmail.com>
2013-06-20 23:41:09 +0200

    Cleaned up variables and docstrings

--- worksheet2/exercise15/exercise15.py
+++ worksheet2/exercise15/exercise15.py
@@ -12,47 +12,63 @@
 
 import time, csv, md5, random
 
+BIT_SIZE   = 20
+NUM_CHAINS = 2**16
+CHAIN_LEN  = 2**8
+TABLE_NAME = "table.csv"
+LOG_FREQ   = 5000
+
+
 def md5_redux(s):
-    """Reduction method to throw away all exceeding output bits from MD5 hashfunction. Outputs 20bit"""
-    return '0x' + md5.new(str(s)).hexdigest()[:5]
+    """MD5 reduction function.
+    Throws away all exceeding output bits from MD5 digest"""
+    return '0x' + md5.new(str(s)).hexdigest()[:BIT_SIZE/4]
 
 def generate_list():
     """Generates a rainbow table"""
     dict = {}
-    order = []
+    order, keys_checked = []
     counter = 0
-    keys_checked = []
-    for i in xrange(0, 2**16 - 1):
-        red = hex(random.getrandbits(20))[:-1]
+    
+    for i in xrange(0, NUM_CHAINS - 1):
+        red = hex(random.getrandbits(BIT_SIZE))[:-1]
         red_start_point = red
         
-        for x in xrange(0, 255):
+        for x in xrange(0, CHAIN_LEN - 1):
             red = md5_redux(red)
             keys_checked.append(red)
 
         red_end_point = red
             
-        if i % 5000 == 0:
+        if i % LOG_FREQ == 0:
            print "Took i calls: %d and the unique keys checked is: %d" % (i, len(set(keys_checked)))
 
         order.append(red_start_point)
         dict[red_start_point] = red_end_point
         counter += 1
+        
     print len(set(keys_checked))
     print len(keys_checked)
+    
     write_to_csv(dict, order)
+
 
 def write_to_csv(dict, order):
     """Writes the rainbow table to a .csv file"""
-    w = csv.writer(open("table.csv", "w"))
+    w = csv.writer(open(TABLE_NAME, 'w'))
     for key in order:
         value = dict[key]
         w.writerow([key, value])
-#    for key, val in dict.items():
-#        w.writerow([key, val])
 
-if __name__ == '__main__':
+
+def main():
     start = time.clock()
-    generate_list() 
+    
+    generate_list()
+    
     end = time.clock()
     print "Took: ", (end - start), " s"    
+
+
+if __name__ == '__main__':
+    main()
