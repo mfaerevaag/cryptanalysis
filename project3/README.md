1946bc09a25f704a77910a95840366e5b3695141
Markus Færevaag <mafaer@gmail.com>
2013-06-22 16:15:25 +0200

    Init breaker

--- project3/README.md
+++ project3/README.md
@@ -1,12 +1,17 @@
 Project 3
 =========
 
-Seconds sine 22. June 2009:
+Seconds sine 22. June 2009 00:00:00:
 
     1245646800
     
 Seconds since 28. June 2009:
 
-    1246165200
+    1246251599
     
+This gives an interval of `604799` seconds.
+
     
+[Source](http://www.unixtimestamp.com/index.php)
+
+
---
+++ project3/breaker.py
@@ -0,0 +1,26 @@
+#
+# Project 3
+#
+# 01435 Practiacal Cryptanalysis
+# Technical University of Denmark
+
+# Markus Faerevaag              (s123692@student.dtu.dk)
+# Christian Mathias Rohde Kiaer (s123812@student.dtu.dk)
+# Jonathan Becktor              (s123094@student.dtu.dk)
+#
+
+
+TIMEINT_START = 1245646800
+TIMEINT_END   = 1246251599
+
+
+def f:
+    
+
+
+def main:
+    #
+
+
+if __name__ == '__main__':
+    main
