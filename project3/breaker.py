9cd43ad438a1184105f5cd46007d268020d312ea
Markus Færevaag <mafaer@gmail.com>
2013-06-22 17:38:41 +0200

    Init k[]

--- project3/breaker.py
+++ project3/breaker.py
@@ -13,14 +13,26 @@
 TIMEINT_START = 1245646800
 TIMEINT_END   = 1246251599
 
+a = 69.069
+c = 5
+m = 2**32
 
-def f:
-    
+
+def update(s):
+    return (a * s + c) % m
 
 
-def main:
-    #
+def main():
+    # Init s as s_0
+    s = TIMEINT_START
+
+    key = []
+    for i in xrange(0, 15):
+        s = update(s)
+        key.append(float.hex(s)[-6:-4])
+
+    print key
 
 
 if __name__ == '__main__':
-    main
+    main()
