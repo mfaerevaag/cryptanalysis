ef4add1ced7408792f8d5408adc42914f608600d
Markus Færevaag <mafaer@gmail.com>
2013-06-23 16:02:05 +0200

    Fixed implementation and added docstrings

--- project3/breaker.py
+++ project3/breaker.py
@@ -9,56 +9,100 @@
 # Jonathan Becktor              (s123094@student.dtu.dk)
 #
 
+import time
 
-TIMEINT_START = 1245646800
-TIMEINT_END   = 1246251599
+START_DATE    = '2009-06-22 00:00:00'
+END_DATE      = '2009-06-28 23:59:59'
+
 CIPHERFILE    = "ciphertext_sheet3.txt"
+PLAINFILE     = 'plaintext.txt'
+KNOWN_PLAINTEXT = ['Snowden',
+                   'Obama',
+                   'Biden',
+                   'Risen',
+                   'Nelson',
+                   'NSA', 'National Security Agency',
+                   'China',
+                   'Whistleblower',
+                   'Hong Kong']
 
-a = 69.069
+# Given constants
+a = 69069
 c = 5
 m = 2**32
 
 
 def update(s):
+    """Update internal state"""
     return (a * s + c) % m
 
 
 def init_key(s):
+    """Init key with internal state"""
     key = []
-    for i in xrange(0, 15):
+    for i in xrange(0, 16):
         s = update(s)
-        key.append(int(float.hex(s)[-6:-4], 16))
+        key.append(str(bin(s)[-8:]))
 
     return key
 
 
 def load_cipher():
+    """Load ciphertext from file"""
     f = open(CIPHERFILE, 'r')
     ciphertext = []
     for line in f:
         for c in line:
             ciphertext.append(c)
-        
+
+    f.close()
     return ciphertext
 
 
 def encrypt(c, key, i):
-#    return ord(c)
-    return int(ord(c)) ^ key[i % 16]
+    """Encrypt character with i in key"""
+    return ord(c) ^ int(key[i % 16], 2)
+
+
+def check_plain(plain):
+    """Check if text contains any of known plaintexts"""
+    for word in KNOWN_PLAINTEXT:
+        if word in plain:
+            return True
+        
+    return False
 
 
 def main():
-    # Init s as s_0
-    key = init_key(TIMEINT_START)
-    
-   # print key
+    # Calculcate time interval
+    date_format = '%Y-%m-%d %H:%M:%S'
+    start = int(time.mktime(time.strptime(START_DATE, date_format))) - time.timezone
+    end = int(time.mktime(time.strptime(END_DATE, date_format))) - time.timezone
 
-    ciphertext = load_cipher()
+    # Start bruteforce search
+    for i in xrange(start, end):
+        key = init_key(i)
 
-#    print ciphertext
+        ciphertext = load_cipher()
 
-    for i in xrange(0, 10):
-        print encrypt(ciphertext[i], key[i], i)
+        plain = ''
+        for j in xrange(0, len(ciphertext)-1):
+            c = chr(encrypt(ciphertext[j], key, j))
+            plain += c
+
+        if check_plain(plain):
+            print "Found plaintext after %i tries!" % (i - start)
+            print "Key: ",
+            for k in key:
+                print chr(int(k, 2)),
+            print "\n-----------------------------------------------------\n"
+            
+            print plain
+            f = open(PLAINFILE, 'w')
+            f.write(plain)
+            f.close()
+            
+            break
 
 
 if __name__ == '__main__':
